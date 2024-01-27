c6fae74748493aecd609b1b60fe3a9c147a44515
fran-00 <fran.mf011@gmail.com>
2024-01-27 15:55:22 +0100

    Add load_api_key method to Google Client to configure api key

--- ai/google.py
+++ ai/google.py
@@ -4,16 +4,12 @@ import google.generativeai as genai
 from dotenv import load_dotenv
 
 
-load_dotenv()
-
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-
 class GoogleClient:
-    
+
     def __init__(self):
         self.model = genai.GenerativeModel('gemini-pro')
         self.chat = self.model.start_chat(history=[])
+        self.load_api_key()
 
     def submit_prompt(self, prompt):
         response = self.chat.send_message(prompt, stream=True)
@@ -21,3 +17,7 @@ class GoogleClient:
         for chunk in response:
             response_text += chunk.text
         return response_text
+
+    def load_api_key(self):
+        load_dotenv()
+        genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
