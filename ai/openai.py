80bcfd9d04efb55eff871ce915f5368f87e025eb
fran-00 <fran.mf011@gmail.com>
2024-01-27 15:10:26 +0100

    Add OpenAIClient class

---
+++ ai/openai.py
@@ -0,0 +1,29 @@
+import os
+
+import openai
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+class OpenAIClient:
+
+    def __init__(self):
+        self.prompts = [{"role": "system", "content": "You are a helpful assistant."}]
+
+    def submit_prompt(self, prompt):
+        self.prompts.append({"role": "user", "content": prompt})
+
+        # Request gpt-3.5-turbo for chat completion
+        response = openai.OpenAI().chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=self.prompts
+        )
+
+        # Print the response and add it to the messages list
+        ai_response = response.choices[0].message.content
+        self.prompts.append({"role": "assistant", "content": ai_response})
+        return ai_response
--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from PySide6.QtWidgets import QApplication
 from core.model import Model, MainThread
 from core.view import View
 from core.controller import Controller
+from ai.openai import OpenAIClient
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     view = View()
-    model = Model()
+    chat = OpenAIClient()
+    model = Model(chat)
     thread = MainThread(model)
     controller = Controller(view, model, thread)
     view.show()
