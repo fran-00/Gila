cbd4d6c814a3ea99f49149c9a9f915216d832782
fran-00 <fran.mf011@gmail.com>
2024-01-27 11:48:14 +0100

    Add MainThread and Model classes

---
+++ core/model.py
@@ -0,0 +1,37 @@
+from PySide6.QtCore import QThread, QObject, Signal, Slot, QEventLoop
+
+
+class MainThread(QThread):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def run(self):
+        self.model.run()
+
+
+class Model(QObject):
+    model_signal_to_controller = Signal(str)
+    player_status_signal = Signal(tuple)
+
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        self.event_loop = QEventLoop()
+
+        while True:
+            self.process_main_loop()
+
+    def process_main_loop(self):
+        self.event_loop.exec()
+        ai_response = "TEST"
+        self.model_signal_to_controller.emit(ai_response)
+
+    @Slot(str)
+    def handle_inbound_signal(self, prompt):
+        self.action = prompt.lower()
+        self.event_loop.exit()
+
+    def handle_outbound_signal(self, ai_response):
+        self.model_signal_to_controller.emit(ai_response)
