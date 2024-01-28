938f5b7a174a1fea3d380bf01cafde5dac993483
fran-00 <fran.mf011@gmail.com>
2024-01-28 14:35:34 +0100

    Add CohereClient

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ from core.view import View
 from core.controller import Controller
 from ai.openai import OpenAIClient
 from ai.google import GoogleClient
+from ai.cohere import CohereClient
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     view = View()
-    client = GoogleClient()
+    client = CohereClient()
+    # client = GoogleClient("gemini-pro")
+    # client = OpenAIClient("gpt-3.5-turbo")
     model = Model(client)
     thread = MainThread(model)
     controller = Controller(view, model, thread)
