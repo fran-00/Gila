cd6180c097b27a8b89fa1c2284e37975152e0e31
fran-00 <fran.mf011@gmail.com>
2024-01-28 14:34:00 +0100

    Add LLMChanger class

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -1,7 +1,23 @@
-from PySide6.QtWidgets import QVBoxLayout, QPushButton
+from PySide6.QtWidgets import QVBoxLayout, QPushButton, QComboBox
 
 
-class LeftSidebar:
+class Sidebar:
 
     def __init__(self, window):
         self.window = window
+        self.sidebar_view = LLMChanger().on_sidebar_widget()
+
+    def on_sidebar_layout(self):
+        sidebar_layout = QVBoxLayout(objectName="sidebar_layout")
+        sidebar_layout.addWidget(self.sidebar_view)
+        return sidebar_layout
+
+
+class LLMChanger:
+
+    def __init__(self):
+        self.sidebar_widget = QComboBox()
+
+    def on_sidebar_widget(self):
+        self.sidebar_widget.addItem("test")
+        return self.sidebar_widget
