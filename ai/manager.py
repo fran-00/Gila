9a96671be2b7092ad17f725cf750db325664cc60
fran-00 <fran.mf011@gmail.com>
2024-01-28 17:04:31 +0100

    Update LLMsManager

--- ai/manager.py
+++ ai/manager.py
@@ -1,6 +1,25 @@
+from .openai import OpenAIClient
+from .google import GoogleClient
+from .cohere import CohereClient
+
+
 class LLMsManager:
+
     def __init__(self):
-        self.current_client = None
+        self.current_client = "GPT-3.5 Turbo"
+        self.llms = {
+            "GPT-4": OpenAIClient("gpt-4"),
+            "GPT-4 Turbo": OpenAIClient("gpt-4-turbo-preview"),
+            "GPT-3.5 Turbo": OpenAIClient("gpt-3.5-turbo-1106"),
+            "Gemini Pro": GoogleClient("gemini-pro"),
+            "Cohere Chat": CohereClient(),
+        }
+
+    def available_models(self):
+        return list(self.llms.keys())
+
+    def set_default_client(self):
+        pass
 
     def set_client(self):
         pass
