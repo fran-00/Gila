f2205093090da45fee3be0d01271311751b8dd6a
fran-00 <fran.mf011@gmail.com>
2024-01-28 13:45:03 +0100

    Update View class to call ChatLog class

--- core/view.py
+++ core/view.py
@@ -1,7 +1,7 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout
 from PySide6.QtCore import Signal, Slot
 
-from ui.chat_log import ChatWidget, PromptLayout
+from ui.chat_log import ChatLog, ChatWidget, PromptLayout
 
 
 class View(QMainWindow):
@@ -18,13 +18,10 @@ class View(QMainWindow):
         """Create a vertical layout for the window"""
         central_widget = QWidget(self)
         self.setCentralWidget(central_widget)
+        self.chat = ChatLog(self)
 
-        self.chat_view = ChatWidget(self).on_chat_widget()
-        self.prompt_layout = PromptLayout(self).on_prompt_layout()
-
-        main_layout = QVBoxLayout(central_widget)
-        main_layout.addWidget(self.chat_view)
-        main_layout.addLayout(self.prompt_layout)
+        main_layout = QHBoxLayout(central_widget)
+        main_layout.addLayout(self.chat.on_chat_layout())
 
     def load_css_file(self):
         with open("ui/styles.css", "r") as file:
@@ -34,11 +31,11 @@ class View(QMainWindow):
         # Emits the signal that contains user prompt
         self.view_signal_to_controller.emit(prompt)
         # Append user prompt to log view window
-        self.chat_view.append(
+        self.chat.chat_view.append(
             f"<p><b>Tu</b>: {prompt}</p>")
 
     @Slot(str)
     def handle_ai_response(self, response):
         """ Slot that receives a string from controller as a signal """
         # Append output to chat view window
-        self.chat_view.append(f"<b>Assistente</b>: {response}")
+        self.chat.chat_view.append(f"<b>Assistente</b>: {response}")
