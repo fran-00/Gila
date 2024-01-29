71fa566ee9869fbed2ce40e08874bf00c32138b7
fran-00 <fran.mf011@gmail.com>
2024-01-29 18:50:09 +0100

    Update launcher to match new controller initialization

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     manager = AIManager()
     view = View()
+    chatlog = view.chat
     sidebar = view.sidebar
     model = Model(manager)
     thread = MainThread(model)
-    controller = Controller(view, model, manager, sidebar, thread)
+    controller = Controller(model, manager, chatlog, sidebar, thread)
     view.show()
 
     sys.exit(app.exec())
