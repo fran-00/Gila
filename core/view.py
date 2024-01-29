15f2f2b0ba93499f4046fd067602f04503646c8b
fran-00 <fran.mf011@gmail.com>
2024-01-29 18:49:22 +0100

    Remove signals and slots from View

--- core/view.py
+++ core/view.py
@@ -1,12 +1,10 @@
 from PySide6.QtWidgets import QMainWindow, QWidget, QHBoxLayout
-from PySide6.QtCore import Signal, Slot
 
 from ui.chat_log import ChatLog
 from ui.sidebar import Sidebar
 
 
 class View(QMainWindow):
-    view_signal_to_controller = Signal(str)
 
     def __init__(self):
         super().__init__()
@@ -29,16 +27,3 @@ class View(QMainWindow):
     def load_css_file(self):
         with open("ui/styles.css", "r") as file:
             return file.read()
-
-    def process_prompt(self, prompt):
-        # Emits the signal that contains user prompt
-        self.view_signal_to_controller.emit(prompt)
-        # Append user prompt to log view window
-        self.chat.chat_view.append(
-            f"<p><b>Tu</b>: {prompt}</p>")
-
-    @Slot(str)
-    def handle_ai_response(self, response):
-        """ Slot that receives a string from controller as a signal """
-        # Append output to chat view window
-        self.chat.chat_view.append(f"<b>Assistente</b>: {response}")
