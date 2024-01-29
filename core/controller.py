a5d2adf8278b4f0a192ff15d81444e9a25c73162
fran-00 <fran.mf011@gmail.com>
2024-01-29 18:47:47 +0100

    Update Controller changing View name to ChatLog to improve readability

--- core/controller.py
+++ core/controller.py
@@ -3,21 +3,22 @@ from PySide6.QtCore import QObject, Signal, Slot
 
 class Controller(QObject):
     controller_signal_to_model = Signal(str)
-    controller_signal_to_view = Signal(str)
     controller_signal_to_manager = Signal(tuple)
+    controller_signal_to_chatlog = Signal(str)
     controller_signal_to_sidebar = Signal(tuple)
 
-    def __init__(self, view, model, manager, sidebar, thread):
+    def __init__(self, model, manager, chatlog, sidebar, thread):
         super().__init__()
 
         model.model_signal_to_controller.connect(self.on_model_signal)
-        view.view_signal_to_controller.connect(self.on_view_signal)
         manager.manager_signal_to_controller.connect(self.on_manager_signal)
+        chatlog.chatlog_signal_to_controller.connect(self.on_chatlog_signal)
         sidebar.sidebar_signal_to_controller.connect(self.on_sidebar_signal)
+
         self.controller_signal_to_model.connect(model.handle_inbound_signal)
-        self.controller_signal_to_view.connect(view.handle_ai_response)
-        self.controller_signal_to_manager.connect(manager.handle_received_signal)
-        self.controller_signal_to_sidebar.connect(sidebar.handle_received_signal)
+        self.controller_signal_to_manager.connect(manager.handle_inbound_signal)
+        self.controller_signal_to_chatlog.connect(chatlog.handle_inbound_signal)
+        self.controller_signal_to_sidebar.connect(sidebar.handle_inbound_signal)
 
         thread.start()
 
@@ -26,15 +27,15 @@ class Controller(QObject):
         """Process data received from the MODEL and send it to VIEW"""
         self.controller_signal_to_view.emit(data)
 
-    @Slot(str)
-    def on_view_signal(self, data):
-        """ Process data received from the VIEW and send it to MODEL"""
-        self.controller_signal_to_model.emit(data)
-
     @Slot(tuple)
     def on_manager_signal(self, data):
         """Process data received from the MANAGER and send it to SIDEBAR"""
         self.controller_signal_to_sidebar.emit(data)
+
+    @Slot(str)
+    def on_chatlog_signal(self, data):
+        """ Process data received from the VIEW and send it to MODEL"""
+        self.controller_signal_to_model.emit(data)
 
     @Slot(tuple)
     def on_sidebar_signal(self, data):
