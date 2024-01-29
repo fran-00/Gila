15381f653c7efb32914830373fb109a77ddaa8b6
fran-00 <fran.mf011@gmail.com>
2024-01-29 18:35:47 +0100

    Remove LLMChanger class and update Sidebar to receive and send signals

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -1,28 +1,42 @@
 from PySide6.QtWidgets import QVBoxLayout, QPushButton, QComboBox
+from PySide6.QtCore import QObject, Signal, Slot
 
-from ai.manager import LLMsManager
 
 
-class Sidebar:
+class Sidebar(QObject):
+    sidebar_signal_to_controller = Signal(tuple)
 
     def __init__(self, window):
+        super().__init__()
         self.window = window
-        self.sidebar_view = LLMChanger().on_sidebar_widget()
+        self.llms = None
+        self.llms_combobox = QComboBox()
 
     def on_sidebar_layout(self):
         sidebar_layout = QVBoxLayout(objectName="sidebar_layout")
-        sidebar_layout.addWidget(self.sidebar_view)
+        # sidebar_layout.addWidget(self.on_llms_combobox())
+        # sidebar_layout.addWidget(self.on_confirm_button())
         return sidebar_layout
 
+    @Slot(tuple)
+    def handle_received_signal(self, data):
+        print(data)
+        self.llms = data
 
-class LLMChanger:
+    def on_combobox_changed(self):
+        pass
 
-    def __init__(self):
-        self.clients_manager = LLMsManager()
-        self.sidebar_widget = QComboBox()
+    def set_client(self):
+        selected_llm = self.llms_combobox.currentText()
+        return self.llms.get(selected_llm)
 
-    def on_sidebar_widget(self):
-        llms_list = self.clients_manager.available_models()
-        for llm in llms_list:
-            self.sidebar_widget.addItem(llm)
-        return self.sidebar_widget
+    def on_llms_combobox(self):
+        for llm in self.llms:
+            self.llms_combobox.addItem(llm)
+        self.llms_combobox.currentIndexChanged.connect(self.on_combobox_changed)
+        return self.llms_combobox
+
+    def on_confirm_button(self):
+        confirm_button = QPushButton("Conferma", self)
+        confirm_button.clicked.connect(self.set_client)
+        return confirm_button
