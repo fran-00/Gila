83029ae7f33c2f09c92ea7cabc54c8be0f915f55
fran-00 <fran.mf011@gmail.com>
2024-01-29 18:34:37 +0100

    Add starter call to manager.handle_received_signal() on Model's run method

--- core/model.py
+++ core/model.py
@@ -20,6 +20,7 @@ class Model(QObject):
 
     def run(self):
         self.event_loop = QEventLoop()
+        self.manager.handle_received_signal()
 
         while True:
             self.process_main_loop()
