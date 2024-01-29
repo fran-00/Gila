499de6416a92ca7042eb9d3ea836d6e9445d820f
fran-00 <fran.mf011@gmail.com>
2024-01-29 18:33:16 +0100

    Update Controller class to connect AIManager and Sidebar via signals and slots

--- core/controller.py
+++ core/controller.py
@@ -4,14 +4,20 @@ from PySide6.QtCore import QObject, Signal, Slot
 class Controller(QObject):
     controller_signal_to_model = Signal(str)
     controller_signal_to_view = Signal(str)
+    controller_signal_to_manager = Signal(tuple)
+    controller_signal_to_sidebar = Signal(tuple)
 
-    def __init__(self, view, model, thread):
+    def __init__(self, view, model, manager, sidebar, thread):
         super().__init__()
 
         model.model_signal_to_controller.connect(self.on_model_signal)
         view.view_signal_to_controller.connect(self.on_view_signal)
+        manager.manager_signal_to_controller.connect(self.on_manager_signal)
+        sidebar.sidebar_signal_to_controller.connect(self.on_sidebar_signal)
         self.controller_signal_to_model.connect(model.handle_inbound_signal)
         self.controller_signal_to_view.connect(view.handle_ai_response)
+        self.controller_signal_to_manager.connect(manager.handle_received_signal)
+        self.controller_signal_to_sidebar.connect(sidebar.handle_received_signal)
 
         thread.start()
 
@@ -24,3 +30,13 @@ class Controller(QObject):
     def on_view_signal(self, data):
         """ Process data received from the VIEW and send it to MODEL"""
         self.controller_signal_to_model.emit(data)
+
+    @Slot(tuple)
+    def on_manager_signal(self, data):
+        """Process data received from the MANAGER and send it to SIDEBAR"""
+        self.controller_signal_to_sidebar.emit(data)
+
+    @Slot(tuple)
+    def on_sidebar_signal(self, data):
+        """Process data received from the SIDEBAR and send it to MANAGER"""
+        self.controller_signal_to_manager.emit(data)
