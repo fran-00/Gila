67824d9039bfdf837a6fe5b1483358587b0c7a90
fran-00 <fran.mf011@gmail.com>
2024-01-29 18:33:34 +0100

    Update AIManager class to send and receive signals

--- ai/manager.py
+++ ai/manager.py
@@ -1,12 +1,16 @@
+from PySide6.QtCore import QObject, Signal, Slot
+
 from .openai import OpenAIClient
 from .google import GoogleClient
 from .cohere import CohereClient
 
 
-class AIManager:
+class AIManager(QObject):
+    manager_signal_to_controller = Signal(tuple)
 
     def __init__(self):
-        self.client = "GPT-3.5 Turbo"
+        super().__init__()
+        self.client = OpenAIClient("gpt-3.5-turbo-1106")
         self.llms = {
             "GPT-4": OpenAIClient("gpt-4"),
             "GPT-4 Turbo": OpenAIClient("gpt-4-turbo-preview"),
@@ -14,9 +18,14 @@ class AIManager:
             "Gemini Pro": GoogleClient("gemini-pro"),
             "Cohere Chat": CohereClient(),
         }
+        
 
-    def available_models(self):
-        return list(self.llms.keys())
+    # def available_models(self):
+    #     return list(self.llms.keys())
 
     def set_client(self, client):
         self.client = client
+
+    @Slot(tuple)
+    def handle_received_signal(self):
+        self.manager_signal_to_controller.emit(list(self.llms.keys()))
