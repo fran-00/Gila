1a93596dffd569577e255b34a4cd8b81b9c54e33
fran-00 <fran.mf011@gmail.com>
2024-02-01 11:02:20 +0100

    Add start_new_chat_to_controller to Sidebar and on_new_chat_button to manage it

--- ui/sidebar.py
+++ ui/sidebar.py
@@ -1,13 +1,15 @@
-from PySide6.QtWidgets import QVBoxLayout, QPushButton, QComboBox
+from PySide6.QtWidgets import QVBoxLayout, QPushButton, QComboBox, QLabel
 from PySide6.QtCore import QObject, Signal, Slot
 
 
 class Sidebar(QObject):
     selected_client_to_controller = Signal(str)
+    start_new_chat_to_controller = Signal()
 
     def __init__(self, window):
         super().__init__()
         self.window = window
+        self.current_llm = None
         self.llms = [
             "GPT-4",
             "GPT-4 Turbo",
@@ -20,6 +22,7 @@ class Sidebar(QObject):
         sidebar_layout = QVBoxLayout(objectName="sidebar_layout")
         sidebar_layout.addWidget(self.on_llms_combobox())
         sidebar_layout.addWidget(self.on_confirm_button())
+        sidebar_layout.addWidget(self.on_new_chat_button())
         return sidebar_layout
 
     def on_llms_combobox(self):
@@ -51,3 +54,11 @@ class Sidebar(QObject):
     def send_selected_client_to_controller(self, llm):
         """ Send a signal to controller """
         self.selected_client_to_controller.emit(llm)
+
+    def on_new_chat_button(self):
+        new_chat_button = QPushButton("Nuova Chat")
+        new_chat_button.clicked.connect(self.send_start_new_chat_to_controller)
+        return new_chat_button
+
+    def send_start_new_chat_to_controller(self):
+        self.start_new_chat_to_controller.emit()
