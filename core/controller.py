3ee5daac127189a70b6a5f86c7e3300a652117bf
fran-00 <fran.mf011@gmail.com>
2024-02-01 09:12:38 +0100

    Update user_prompt and ai_response signals and slots names in Controller class

--- core/controller.py
+++ core/controller.py
@@ -2,8 +2,8 @@ from PySide6.QtCore import QObject, Signal, Slot
 
 
 class Controller(QObject):
-    controller_signal_to_model = Signal(str)
-    controller_signal_to_chatlog = Signal(str)
+    user_prompt_to_model = Signal(str)
+    ai_response_to_chatlog = Signal(str)
 
     controller_signal_to_manager_llm = Signal(str)
     controller_signal_to_sidebar_llm = Signal(str)
@@ -18,32 +18,30 @@ class Controller(QObject):
     def connect_signals_and_slots(self):
         self.model.ai_response_signal_to_controller.connect(self.on_ai_response_signal)
         self.view.chat.user_prompt_signal_to_controller.connect(self.on_user_prompt_signal)
+        self.model.manager.manager_signal_to_controller_llm.connect(self.on_client_from_manager_signal)
+        self.view.sidebar.sidebar_signal_to_controller_llm.connect(self.on_change_client_from_sidebar_signal)
 
-        self.model.manager.manager_signal_to_controller_llm.connect(self.on_manager_signal_llm)
-        self.view.sidebar.sidebar_signal_to_controller_llm.connect(self.on_sidebar_signal_llm)
-
-        self.controller_signal_to_model.connect(self.model.handle_user_prompt)
-        self.controller_signal_to_chatlog.connect(self.view.chat.handle_ai_response)
-
+        self.user_prompt_to_model.connect(self.model.handle_user_prompt)
+        self.ai_response_to_chatlog.connect(self.view.chat.handle_ai_response)
         self.controller_signal_to_manager_llm.connect(self.model.manager.handle_inbound_llm_signal)
         self.controller_signal_to_sidebar_llm.connect(self.view.sidebar.handle_inbound_llm_signal)
 
     @Slot(str)
     def on_ai_response_signal(self, ai_response):
         """Process AI response received from the MODEL and send it to CHATLOG"""
-        self.controller_signal_to_chatlog.emit(ai_response)
+        self.ai_response_to_chatlog.emit(ai_response)
 
     @Slot(str)
     def on_user_prompt_signal(self, user_prompt):
         """ Process user prompt received from the CHATLOG and send it to MODEL"""
-        self.controller_signal_to_model.emit(user_prompt)
+        self.user_prompt_to_model.emit(user_prompt)
 
     @Slot(str)
-    def on_manager_signal_llm(self, llm):
+    def on_client_from_manager_signal(self, llm):
         """Process data received from the MANAGER and send it to SIDEBAR"""
         self.controller_signal_to_sidebar_llm.emit(llm)
 
     @Slot(str)
-    def on_sidebar_signal_llm(self, llm):
+    def on_change_client_from_sidebar_signal(self, llm):
         """Process data received from the SIDEBAR and send it to MANAGER"""
         self.controller_signal_to_manager_llm.emit(llm)
