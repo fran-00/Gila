d5868196bdac11a0081372b00210decacffd82e5
fran-00 <fran.mf011@gmail.com>
2024-02-02 09:33:19 +0100

    Add chat_stopped_to_model signal and on_stop_chat_from_sidebar_signal method

--- core/controller.py
+++ core/controller.py
@@ -7,6 +7,7 @@ class Controller(QObject):
     selected_client_to_manager = Signal(str)
     current_client_to_sidebar = Signal(str)
     new_chat_started_to_model = Signal()
+    chat_stopped_to_model = Signal()
 
     def __init__(self, model, view, thread):
         super().__init__()
@@ -20,13 +21,15 @@ class Controller(QObject):
         self.view.chat.user_prompt_signal_to_controller.connect(self.on_user_prompt_signal)
         self.model.manager.manager_signal_to_controller_llm.connect(self.on_client_from_manager_signal)
         self.view.sidebar.selected_client_to_controller.connect(self.on_change_client_from_sidebar_signal)
-        self.view.sidebar.start_new_chat_to_controller.connect(self.on_start_new_chat_from_sidebar_signal)
+        # self.view.sidebar.start_new_chat_to_controller.connect(self.on_start_new_chat_from_sidebar_signal)
+        self.view.sidebar.stop_chat_to_controller.connect(self.on_stop_chat_from_sidebar_signal)
 
         self.user_prompt_to_model.connect(self.model.get_user_prompt_from_controller)
         self.ai_response_to_chatlog.connect(self.view.chat.get_ai_response_from_controller)
         self.selected_client_to_manager.connect(self.model.manager.get_new_client_from_controller)
         self.current_client_to_sidebar.connect(self.view.sidebar.get_current_client_from_controller)
         self.new_chat_started_to_model.connect(self.model.new_chat_started_from_controller)
+        self.chat_stopped_to_model.connect(self.model.chat_stopped_from_controller)
 
     @Slot(str)
     def on_ai_response_signal(self, ai_response):
@@ -49,7 +52,12 @@ class Controller(QObject):
         self.selected_client_to_manager.emit(new_client)
 
     @Slot()
-    def on_start_new_chat_from_sidebar_signal(self):
+    def on_stop_chat_from_sidebar_signal(self):
+        self.chat_stopped_to_model.emit()
         self.view.chat.chat_widget.clear()
-        self.model.client.stream_stopped = True
+        self.model.manager.stream_stopped = True
         print("> ChatLog cleared.")
+
+    @Slot()
+    def on_start_new_chat_from_sidebar_signal(self):
+        pass
