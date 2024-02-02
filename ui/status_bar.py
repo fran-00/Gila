5ca27dad2d789874f3a37745c190107fad3a679b
fran-00 <fran.mf011@gmail.com>
2024-02-02 11:49:20 +0100

    Add messages_history to StatusBar

--- ui/status_bar.py
+++ ui/status_bar.py
@@ -8,7 +8,9 @@ class StatusBar(QObject):
         super().__init__()
         self.window = window
         self.status_bar = QStatusBar()
+        self.messages_history = []
 
     @Slot(str)
     def on_status_update(self, status):
+        self.messages_history.append(status)
         self.status_bar.showMessage(status)
