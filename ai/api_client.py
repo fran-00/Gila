fcc44fe17a54fb8eb17e39ae770af2f441050488
fran-00 <fran.mf011@gmail.com>
2024-02-06 12:26:27 +0100

    Add check_if_api_key and get_api_key methods to APIClient

--- ai/api_client.py
+++ ai/api_client.py
@@ -10,12 +10,14 @@ class APIClient:
         self.llm_name = None
         self.temperature = None
         self.chat_messages = None
+        self.api_key = None
 
-    def load_api_key(self, name):
+    def check_if_api_key(self, name):
         load_dotenv()
-        api_key = os.getenv(f"{name}_API_KEY")
-        if api_key:
-            print("API Key set")
-            return api_key
-        else:
-            print("No API Key set")
+        if key := os.getenv(f"{name}_API_KEY"):
+            self.api_key = key
+            return True
+        return False
+
+    def get_api_key(self):
+        return self.api_key
