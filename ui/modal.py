0e2ece280ad5a539734d95711d432eb76704473d
fran-00 <fran.mf011@gmail.com>
2024-02-06 18:59:48 +0100

    Add load_css_file to MissingAPIKeyModal

--- ui/modal.py
+++ ui/modal.py
@@ -7,9 +7,14 @@ class MissingAPIKeyModal(QDialog):
         super().__init__()
         self.window = window
         self.setWindowTitle("Chiave Mancante")
+        self.setStyleSheet(self.load_css_file())
         layout = QVBoxLayout(self)
         self.line_edit = QLineEdit(self)
         layout.addWidget(self.line_edit)
         button = QPushButton("OK", self)
         button.clicked.connect(self.accept)
         layout.addWidget(button)
+
+    def load_css_file(self):
+        with open("ui/styles.css", "r") as file:
+            return file.read()
