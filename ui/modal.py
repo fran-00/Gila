a7b3d161f9875b0e95b9b46e476b295ca14d0ab5
fran-00 <fran.mf011@gmail.com>
2024-02-06 19:07:22 +0100

    Add on_modal_layout method to MissingAPIKeyModal, add a label with text to modal

--- ui/modal.py
+++ ui/modal.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QDialog, QVBoxLayout, QLineEdit, QPushButton
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QLabel, QLineEdit, QPushButton
 
 
 class MissingAPIKeyModal(QDialog):
@@ -8,13 +8,18 @@ class MissingAPIKeyModal(QDialog):
         self.window = window
         self.setWindowTitle("Chiave Mancante")
         self.setStyleSheet(self.load_css_file())
-        layout = QVBoxLayout(self)
-        self.line_edit = QLineEdit(self)
-        layout.addWidget(self.line_edit)
-        button = QPushButton("OK", self)
-        button.clicked.connect(self.accept)
-        layout.addWidget(button)
+        self.on_modal_layout()
 
     def load_css_file(self):
         with open("ui/styles.css", "r") as file:
             return file.read()
+
+    def on_modal_layout(self):
+        self.modal_layout = QVBoxLayout(self)
+        modal_text = QLabel(f"Inserisci l'API Key")
+        self.modal_layout.addWidget(modal_text)
+        self.modal_entry_line = QLineEdit(self)
+        self.modal_layout.addWidget(self.modal_entry_line)
+        modal_button = QPushButton("OK", self)
+        modal_button.clicked.connect(self.accept)
+        self.modal_layout.addWidget(modal_button)
