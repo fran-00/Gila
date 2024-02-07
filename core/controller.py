f9a76557a3588c0d0754ebfdb8e0586c5d7ed57c
fran-00 <fran.mf011@gmail.com>
2024-02-07 14:37:00 +0100

    Update Controller formatting

--- core/controller.py
+++ core/controller.py
@@ -24,31 +24,44 @@ class Controller(QObject):
         # Connect CONTROLLER's signals to MODEL's slots
         self.user_prompt_to_model.connect(self.model.get_user_prompt_slot)
         self.chat_stopped_to_model.connect(self.model.chat_stopped_slot)
-        self.selected_client_to_manager.connect(self.model.manager.get_new_client_slot)
+        self.selected_client_to_manager.connect(
+            self.model.manager.get_new_client_slot)
         self.api_key_to_manager.connect(self.model.manager.api_key_slot)
 
         # Connect MODEL's signals to CONTROLLER's slots
-        self.model.ai_response_signal_to_controller.connect(self.ai_response_slot)
-        self.model.start_new_chat_to_controller.connect(self.new_chat_started_slot)
-        self.model.manager.api_key_is_valid_to_controller.connect(self.api_key_is_valid_slot)
+        self.model.ai_response_signal_to_controller.connect(
+            self.ai_response_slot)
+        self.model.start_new_chat_to_controller.connect(
+            self.new_chat_started_slot)
+        self.model.manager.api_key_is_valid_to_controller.connect(
+            self.api_key_is_valid_slot)
 
     def connect_view(self):
         # Connect CONTROLLER's signals to VIEW's slots
-        self.ai_response_to_chatlog.connect(self.view.chat.get_ai_response_slot)
-        self.update_status_bar.connect(self.view.status_bar.on_status_update_slot)
-        self.missing_api_key_to_view.connect(self.view.on_missing_key_modal_slot)
-        self.api_key_is_valid_to_view.connect(self.view.modal.on_api_key_validation_slot)
+        self.ai_response_to_chatlog.connect(
+            self.view.chat.get_ai_response_slot)
+        self.update_status_bar.connect(
+            self.view.status_bar.on_status_update_slot)
+        self.missing_api_key_to_view.connect(
+            self.view.on_missing_key_modal_slot)
+        self.api_key_is_valid_to_view.connect(
+            self.view.modal.on_api_key_validation_slot)
 
         # Connect VIEW's signals to CONTROLLER's slots
-        self.view.sidebar.selected_client_to_controller.connect(self.client_changed_from_sidebar_slot)
-        self.view.sidebar.stop_chat_to_controller.connect(self.chat_stopped_from_sidebar_slot)
-        self.view.chat.user_prompt_signal_to_controller.connect(self.user_prompt_slot)
-        self.view.chat.start_new_chat_to_controller.connect(self.new_chat_started_slot)
-        self.view.modal.api_key_to_controller.connect(self.api_key_from_modal_slot)
-        
-        # Connect ChatLog to Status Bar
-        self.view.chat.update_status_bar_from_chatlog.connect(self.view.status_bar.on_status_update_slot)
+        self.view.sidebar.selected_client_to_controller.connect(
+            self.client_changed_from_sidebar_slot)
+        self.view.sidebar.stop_chat_to_controller.connect(
+            self.chat_stopped_from_sidebar_slot)
+        self.view.chat.user_prompt_signal_to_controller.connect(
+            self.user_prompt_slot)
+        self.view.chat.start_new_chat_to_controller.connect(
+            self.new_chat_started_slot)
+        self.view.modal.api_key_to_controller.connect(
+            self.api_key_from_modal_slot)
 
+        # Connect ChatLog to Status Bar
+        self.view.chat.update_status_bar_from_chatlog.connect(
+            self.view.status_bar.on_status_update_slot)
 
     @Slot(str)
     def ai_response_slot(self, ai_response):
@@ -58,11 +71,12 @@ class Controller(QObject):
         Emits two signals:
             - ai_response_to_chatlog (view.chat.get_ai_response_slot)
             - update_status_bar (view.status_bar.on_status_update_slot)
-        
+
         Receive AI response from the MODEL and send it to CHATLOG
         """
         self.ai_response_to_chatlog.emit(ai_response)
-        self.update_status_bar.emit("Risposta ricevuta. In attesa di un nuovo messaggio...")
+        self.update_status_bar.emit(
+            "Risposta ricevuta. In attesa di un nuovo messaggio...")
 
     @Slot(str)
     def user_prompt_slot(self, user_prompt):
@@ -115,7 +129,8 @@ class Controller(QObject):
         """
         self.model.manager.stream_stopped = False
         self.update_status_bar.emit("Nuova conversazione avviata.")
-        self.view.sidebar.update_settings_label(self.model.manager.on_current_settings())
+        self.view.sidebar.update_settings_label(
+            self.model.manager.on_current_settings())
         if self.model.manager.on_api_key() is False:
             self.missing_api_key_to_view.emit()
         else:
