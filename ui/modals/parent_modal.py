923ae2fd94867136f05ce952aba5b404e74fa2a0
fran-00 <fran.mf011@gmail.com>
2024-02-07 17:14:10 +0100

    Add new modals module and a parent Modal class

---
+++ ui/modals/parent_modal.py
@@ -0,0 +1,18 @@
+from PySide6.QtWidgets import QDialog
+from PySide6.QtCore import Signal
+
+
+class Modal(QDialog):
+    api_key_to_controller = Signal(str)
+
+    def __init__(self, window):
+        super().__init__()
+        self.window = window
+        self.resize(300, 300)
+        self.setStyleSheet(self.load_css_file())
+        self.on_modal_layout()
+
+    def load_css_file(self):
+        """ Loads CSS File to apply style to Modal Window """
+        with open("ui/modals/modals_styles.css", "r") as file:
+            return file.read()
