1d75e728e4bce88028a29ff7540c9752c350d5ad
fran-00 <fran.mf011@gmail.com>
2024-02-07 17:45:17 +0100

    Add on_client_list_row method

--- ui/modals/manage_api_keys_modal.py
+++ ui/modals/manage_api_keys_modal.py
@@ -1,8 +1,8 @@
 import os
 
 from dotenv import load_dotenv
-from PySide6.QtWidgets import QVBoxLayout, QLabel, QLineEdit, QPushButton
-from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+from PySide6.QtGui import QIcon
 
 from .parent_modal import Modal
 
@@ -19,6 +19,9 @@ class ManageAPIKeysModal(Modal):
     def on_modal_layout(self):
         """ Creates modal layout and calls methods that adds widgets """
         self.modal_layout = QVBoxLayout(self)
+        self.on_client_list_row("OpenAI")
+        self.on_client_list_row("Google")
+        self.on_client_list_row("Cohere")
 
     def on_stored_api_keys(self):
         load_dotenv()
@@ -28,3 +31,17 @@ class ManageAPIKeysModal(Modal):
             self.openai_key = True
         if os.getenv(f"COHERE_API_KEY"):
             self.openai_key = True
+
+    def on_client_list_row(self, client_name):
+        row_layout = QHBoxLayout()
+        label = QLabel(client_name)
+        row_layout.addWidget(label)
+
+        icon = QIcon("ui/icons/green-circle.png")
+        icon_label = QLabel()
+        icon_label.setPixmap(icon.pixmap(24, 24))
+        row_layout.addWidget(icon_label)
+
+        modify_button = QPushButton("Modifica")
+        row_layout.addWidget(modify_button)
+        self.modal_layout.addLayout(row_layout)
