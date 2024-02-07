0601560bcc24155e4c739813f330c460afe06f3d
fran-00 <fran.mf011@gmail.com>
2024-02-07 17:24:59 +0100

    Add on_stored_api_keys method to ManageAPIKeysModal

--- ui/modals/manage_api_keys.py
+++ ui/modals/manage_api_keys.py
@@ -1,3 +1,6 @@
+import os
+
+from dotenv import load_dotenv
 from PySide6.QtWidgets import QVBoxLayout, QLabel, QLineEdit, QPushButton
 from PySide6.QtCore import Slot
 
@@ -9,7 +12,19 @@ class ManageAPIKeysModal(Modal):
     def __init__(self, window):
         super().__init__(window)
         self.setWindowTitle("Gestisci Chiavi API")
+        self.openai_key = False
+        self.google_key = False
+        self.cohere_key = False
 
     def on_modal_layout(self):
         """ Creates modal layout and calls methods that adds widgets """
         self.modal_layout = QVBoxLayout(self)
+
+    def on_stored_api_keys(self):
+        load_dotenv()
+        if os.getenv(f"OPENAI_API_KEY"):
+            self.openai_key = True
+        if os.getenv(f"GOOGLE_API_KEY"):
+            self.openai_key = True
+        if os.getenv(f"COHERE_API_KEY"):
+            self.openai_key = True
