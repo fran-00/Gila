2f1888e24286e1de883489aee0d804eb8892b677
fran-00 <fran.mf011@gmail.com>
2024-02-07 14:49:22 +0100

    Add docstrings to MissingAPIKeyModal's methods

--- ui/modal.py
+++ ui/modal.py
@@ -14,10 +14,12 @@ class MissingAPIKeyModal(QDialog):
         self.on_modal_layout()
 
     def load_css_file(self):
+        """ Loads CSS File to apply style to Modal Window """
         with open("ui/styles.css", "r") as file:
             return file.read()
 
     def on_modal_layout(self):
+        """ Creates modal layout and calls methods that adds widgets """
         self.modal_layout = QVBoxLayout(self)
         self.on_modal_entry_line()
         self.on_modal_button()
@@ -26,33 +28,39 @@ class MissingAPIKeyModal(QDialog):
         self.on_key_is_not_valid_label()
 
     def on_modal_entry_line(self):
+        """ Add instructions text and an entry line """
         modal_text = QLabel(f"Inserisci l'API Key, verrà inviata per la verifica.")
         self.modal_layout.addWidget(modal_text)
         self.modal_entry_line = QLineEdit(self)
         self.modal_layout.addWidget(self.modal_entry_line)
 
     def on_modal_button(self):
+        """ Add a button so send API Key """
         self.modal_button = QPushButton("Invia", self)
         self.modal_button.clicked.connect(self.process_api_key)
         # modal_button.clicked.connect(self.accept)
         self.modal_layout.addWidget(self.modal_button)
 
     def on_modal_wait_label(self):
+        """ Add a label when user must wait for API response """
         self.wait_label = QLabel('Attendere...')
         self.wait_label.hide()
         self.modal_layout.addWidget(self.wait_label)
 
     def on_key_is_valid_label(self):
+        """ Add a label that says that API key is valid """
         self.key_valid_label = QLabel('La chiave inserita è valida ed è stata registata.')
         self.key_valid_label.hide()
         self.modal_layout.addWidget(self.key_valid_label)
 
     def on_key_is_not_valid_label(self):
+        """ Add a label that says that API key is NOT valid """
         self.key_not_valid_label = QLabel('La chiave inserita non è valida, prova di nuovo o chiudi.')
         self.key_not_valid_label.hide()
         self.modal_layout.addWidget(self.key_not_valid_label)
 
     def process_api_key(self):
+        """ Hides validation labels, gets API key and sends it as a Signal """
         self.key_valid_label.hide()
         self.key_not_valid_label.hide()
         api_key = self.modal_entry_line.text().strip()
