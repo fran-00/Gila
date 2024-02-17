8b4168a043ef1ccfd973d92be8ffa6bd570f4e9f
fran-00 <fran.mf011@gmail.com>
2024-02-17 15:43:17 +0100

    Remove comment lines

--- ai/openai.py
+++ ai/openai.py
@@ -17,13 +17,11 @@ class OpenAIClient(APIClient):
     def submit_prompt(self, prompt):
         self.chat_messages.append({"role": "user", "content": prompt})
 
-        # Request gpt-3.5-turbo for chat completion
         response = openai.OpenAI().chat.completions.create(
             model=self.llm,
             messages=self.chat_messages
         )
 
-        # Print the response and add it to the messages list
         ai_response = response.choices[0].message.content
         self.chat_messages.append({"role": "assistant", "content": ai_response})
         return ai_response
