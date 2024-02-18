2ffe4149357b9443e628b443b0a767a834ffedcb
fran-00 <fran.mf011@gmail.com>
2024-02-18 19:36:13 +0100

    Update main.py to show loading screen on startup

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import sys
+import time
 
 from PySide6.QtWidgets import QApplication
 
 from core.model import Model, MainThread
-from ui.view import View
+from ui.view import View, LoadingScreen
 from core.controller import Controller
 from ai.manager import AIManager
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    splash = LoadingScreen()
+    splash.show()
+    time.sleep(2)
+
     manager = AIManager()
     view = View()
     model = Model(manager)
@@ -17,4 +23,5 @@ if __name__ == '__main__':
     controller = Controller(model, view, thread)
     view.show()
 
+    splash.finish(view)
     sys.exit(app.exec())
