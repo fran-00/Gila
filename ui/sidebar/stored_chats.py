ad94954b877b426cf08eb8c8b5d1ee9c7d1eee7c
fran-00 <fran.mf011@gmail.com>
2024-02-21 17:54:06 +0100

    Fix StoredChats not creating buttons correctly

--- ui/sidebar/stored_chats.py
+++ ui/sidebar/stored_chats.py
@@ -13,16 +13,20 @@ class StoredChats(QObject):
         super().__init__()
         self.parent_widget = parent_widget
         self.widget_container = QWidget(objectName="stored_chats_widget")
+        self.chatlog = None
         self.on_stored_chats_layout()
 
     def on_stored_chats_layout(self):
-        stored_chats_layout = QVBoxLayout(self.widget_container)
+        self.stored_chats_layout = QVBoxLayout(self.widget_container)
         
         chats = os.listdir("storage/saved_data")
         for file in chats:
-            button = QPushButton(file)
-            stored_chats_layout.addWidget(button)
-            button.clicked.connect(lambda: self.on_load_saved_chat(file))
+            self.add_stored_chat_button(file)
+
+    def add_stored_chat_button(self, filename):
+        button = QPushButton(filename, objectName=f"{filename}_button")
+        button.clicked.connect(lambda: self.on_load_saved_chat(filename))
+        self.stored_chats_layout.addWidget(button)
 
     def on_load_saved_chat(self, file_name):
         """
@@ -30,14 +34,8 @@ class StoredChats(QObject):
         otherwise they won't fit in a single signal if sent from here
         """
         chat_id = re.sub(r'\.pk$', '', file_name)
-        self.loading_saved_chat_id_to_controller.emit(chat_id)
-
         with open(f'storage/saved_data/{chat_id}.pk', 'rb') as file:
             saved_data = pickle.load(file)
             chat = saved_data[chat_id]
-            # client = chat["client"]
-            # temperature = chat["temperature"]
-            # chat_history = chat["chat_history"]
-            # client = chat["client"]
-            chatlog = chat["chat_log"]
-            print(chatlog)
+            self.chatlog = chat["chat_log"]
+        self.loading_saved_chat_id_to_controller.emit(chat_id)
