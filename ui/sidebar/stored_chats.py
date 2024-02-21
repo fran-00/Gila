efbdf91f7dab50c5686408a50235d4b4569bf706
fran-00 <fran.mf011@gmail.com>
2024-02-21 14:16:59 +0100

    Change signal name to loading_saved_chat_id_to_controller

--- ui/sidebar/stored_chats.py
+++ ui/sidebar/stored_chats.py
@@ -1,11 +1,13 @@
 import os
+import re
+import pickle
 
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QLabel
 from PySide6.QtCore import QObject, Signal
 
 
 class StoredChats(QObject):
-    loading_saved_chat_to_controller = Signal(str)
+    loading_saved_chat_id_to_controller = Signal(str)
 
     def __init__(self, parent_widget):
         super().__init__()
@@ -22,5 +24,18 @@ class StoredChats(QObject):
             stored_chats_layout.addWidget(button)
             button.clicked.connect(lambda: self.on_load_saved_chat(file))
 
-    def on_load_saved_chat(self, file):
-        pass
+    def on_load_saved_chat(self, file_name):
+        """
+        MUST ONLY restore chatlog, other data must be parsed directly from manager on signal receiving
+        otherwise they won't fit in a single signal if sent from here
+        """
+        chat_id = re.sub(r'\.pk$', '', file_name)
+        with open(f'storage/saved_data/{chat_id}.pk', 'rb') as file:
+            saved_data = pickle.load(file)
+            chat = saved_data[chat_id]
+            # client = chat["client"]
+            # temperature = chat["temperature"]
+            # chat_history = chat["chat_history"]
+            # client = chat["client"]
+            chatlog = chat["chat_log"]
+            print(chatlog)
