99d60a0a2d7e246178dceaba8617a2405670c336
fran-00 <fran.mf011@gmail.com>
2024-02-27 08:53:46 +0100

    Change Sidebar's widget parameter to Sidebar's widget instead of Sidebar's self

--- gila/ui/sidebar/parent_sidebar.py
+++ gila/ui/sidebar/parent_sidebar.py
@@ -12,8 +12,8 @@ class Sidebar(QObject):
         super().__init__()
         self.window = window
         self.widget_container = QWidget(objectName="sidebar_container")
-        self.stored_chats = StoredChats(self)
-        self.current_settings = CurrentSettings(self)
+        self.stored_chats = StoredChats(self.widget_container)
+        self.current_settings = CurrentSettings(self.widget_container)
         self.change_settings_modal = ChangeSettingsModal(self.window, self.current_settings)
         self.on_sidebar_container()
 
