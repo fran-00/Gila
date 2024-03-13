0e58b9f168d7d6017eb6b6c7605dfd652f3aa093
fran-00 <fran.mf011@gmail.com>
2024-03-13 18:33:23 +0100

    Add ConfirmChatDeletionModal class to modals module

---
+++ gila/ui/modals/confirm_chat_deletion_modal.py
@@ -0,0 +1,31 @@
+from PySide6.QtWidgets import QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+
+from .parent_modal import Modal
+
+
+class ConfirmChatDeletionModal(Modal):
+
+    def __init__(self, window):
+        super().__init__(window)
+        self.setWindowTitle("Cancellazione Chat")
+        self.on_modal_layout()
+
+    def on_modal_layout(self):
+        """ Creates modal layout and calls methods that adds widgets """
+        self.modal_layout = QVBoxLayout(self)
+        self.modal_text = QLabel("Sei sicuro di voler eliminare questa chat? Non sarà più possibile recuperarla!")
+        self.modal_layout.addWidget(self.modal_text)
+        self.buttons_layout = QHBoxLayout()
+        self.modal_layout.addLayout(self.buttons_layout)
+        self.on_confirm_button()
+        self.on_refuse_button()
+
+    def on_confirm_button(self):
+        self.modal_button = QPushButton("Sì, elimina!", self)
+        self.modal_button.clicked.connect(self.accept)
+        self.buttons_layout.addWidget(self.modal_button)
+
+    def on_refuse_button(self):
+        self.modal_button = QPushButton("No, annulla", self)
+        self.modal_button.clicked.connect(self.accept)
+        self.buttons_layout.addWidget(self.modal_button)
