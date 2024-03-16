186164470de2502d943856c949e51d508d1687f6
fran-00 <fran.mf011@gmail.com>
2024-03-16 20:05:43 +0100

    Update ChangeSettingsModal's methods

--- gila/ui/modals/change_settings_modal.py
+++ gila/ui/modals/change_settings_modal.py
@@ -19,27 +19,22 @@ class ChangeSettingsModal(Modal):
         self.modal_text = QLabel("Modifica le impostazioni, saranno applicate all'avvio di una nuova chat.")
         self.modal_text.setWordWrap(True)
         self.modal_layout.addWidget(self.modal_text)
-        self.modal_layout.addWidget(self.on_llms_combobox())
-        self.modal_layout.addWidget(self.on_confirm_button())
+        self.on_llms_combobox()
+        self.on_confirm_button()
 
     def on_llms_combobox(self):
         """ Creates ComboBox with llms list """
         self.llms_combobox = QComboBox()
         for llm in self.current_settings.llms:
             self.llms_combobox.addItem(llm)
-        self.llms_combobox.currentIndexChanged.connect(
-            self.on_combobox_changed)
-        return self.llms_combobox
-
-    def on_combobox_changed(self):
-        pass
+        self.modal_layout.addWidget(self.llms_combobox)
 
     def on_confirm_button(self):
         """ Creates button to confirm llm selection """
         confirm_button = QPushButton("Conferma")
         confirm_button.clicked.connect(self.send_selected_client_to_controller)
         confirm_button.clicked.connect(self.accept)
-        return confirm_button
+        self.modal_layout.addWidget(confirm_button)
 
     def send_selected_client_to_controller(self):
         """ Sends selected llm to controller: signal is triggered when Confirm
