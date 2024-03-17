bddc2983439ae1c828c2c07e955c3da8707e1616
fran-00 <fran.mf011@gmail.com>
2024-03-17 19:13:24 +0100

    Add view.sidebar.on_show_widgets() call to new_chat_started_slot

--- gila/core/controller.py
+++ gila/core/controller.py
@@ -163,6 +163,7 @@ class Controller(QObject):
             else:
                 self.view.on_show_chatlog_and_prompt_line()
                 self.view.sidebar.current_settings.on_show_widgets()
+                self.view.sidebar.on_show_widgets()
             self.model.running = True
             self.main_thread.model.run()
             return
