c689e6050a15713928b70913199f93ab472d820f
fran-00 <fran.mf011@gmail.com>
2024-03-17 15:55:56 +0100

    Update ManageAPIKeysModal to call on_modal_text parent Modal's method

--- gila/ui/modals/manage_api_keys_modal.py
+++ gila/ui/modals/manage_api_keys_modal.py
@@ -22,9 +22,8 @@ class ManageAPIKeysModal(Modal):
     def on_modal_layout(self):
         """ Creates modal layout and calls methods that adds widgets """
         self.modal_layout = QVBoxLayout(self)
-        self.modal_text = QLabel("Modifica le API Key salvate (pallino verde) o aggiungi quelle mancanti (pallino rosso).")
-        self.modal_text.setWordWrap(True)
-        self.modal_layout.addWidget(self.modal_text)
+        self.on_modal_text()
+        self.modal_text.setText("Modifica le API Key salvate (pallino verde) o aggiungi quelle mancanti (pallino rosso).")
         self.set_icons()
         for key in self.api_keys.keys():
             self.on_client_list_row(key)
