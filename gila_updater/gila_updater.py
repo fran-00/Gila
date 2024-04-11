c1676d9b12fbc82f6b4cfc1793c9aecfabd8d82c
fran-00 <fran.mf011@gmail.com>
2024-04-11 10:16:17 +0200

    Add compare_local_and_remote_sha method to GilaUpdater

--- gila_updater/gila_updater.py
+++ gila_updater/gila_updater.py
@@ -9,3 +9,15 @@ class GilaUpdater:
 
     def __init__(self):
         self.repo_url = "https://github.com/fran-00/gila.git"
+
+    def compare_local_and_remote_sha(self):
+        with open('storage/gila_conf.json', 'r') as f:
+            data = json.load(f)
+        local_sha = data["local_sha"]
+        process = subprocess.Popen(["git", "ls-remote", self.repo_url], stdout=subprocess.PIPE)
+        stdout, stderr = process.communicate()
+        remote_sha = re.split(r'\t+', stdout.decode('ascii'))[0]
+        if local_sha != remote_sha:
+            print("Codebase is not updated!")
+        else:
+            print("Codebase is updated!")
