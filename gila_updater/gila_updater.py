a407ca116434f6b1efb230cdf59ff94783205955
fran-00 <fran.mf011@gmail.com>
2024-04-12 18:23:35 +0200

    Add clone_repo method to GilaUpdater

--- gila_updater/gila_updater.py
+++ gila_updater/gila_updater.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 import sys
 import venv
@@ -25,3 +26,23 @@ class GilaUpdater:
     def create_virtualenv(self, env_dir):
         venv.create(env_dir, system_site_packages=False, clear=True)
         print(f"Ambiente virtuale creato con successo in: {env_dir}")
+
+    def clone_repo(self):
+        repo = Repo.clone_from(self.repo_url, self.local_dir)
+        env_dir = os.path.join(self.local_dir, 'venv')
+        self.create_virtualenv(env_dir)
+        requirements_file = os.path.join('/storage/cloned_repo/', 'requirements.txt')
+        if os.path.exists(requirements_file):
+            self.install_requirements(env_dir, requirements_file)
+        try:
+
+            os.chdir(self.local_dir)
+            activate_script = os.path.join(
+                env_dir, 'Scripts' if sys.platform == 'win32' else 'bin', 'activate')
+            subprocess.run([activate_script, '&&', 'pyinstaller',
+                           'build.spec'], shell=True, check=True)
+            # shutil.rmtree('build')
+            # shutil.rmtree('nome_script.spec')
+            print("Build completata con successo!")
+        except Exception as e:
+            print(f"Si è verificato un errore durante la build: {e}")
