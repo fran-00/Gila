033c098d25ed474dab8ed21e327aace1d27f1cef
fran-00 <fran.mf011@gmail.com>
2024-04-12 18:21:28 +0200

    Add install_requirements method to GilaUpdater

--- gila_updater/gila_updater.py
+++ gila_updater/gila_updater.py
@@ -1,6 +1,6 @@
-import json
-import re
+import os
 import subprocess
+import sys
 
 from git import Repo
 
@@ -10,3 +10,13 @@ class GilaUpdater:
     def __init__(self):
         self.repo_url = "https://github.com/fran-00/gila.git"
         self.local_dir = "storage/cloned_repo"
+
+    def install_requirements(self, requirements_file):
+        activate_script = os.path.join(
+            self.local_dir, 'Scripts' if sys.platform == 'win32' else 'bin', 'activate')
+        try:
+            subprocess.run([activate_script, '&&', 'pip', 'install',
+                           '-r', requirements_file], shell=False, check=True)
+            print("Pacchetti di terze parti installati con successo!")
+        except Exception as e:
+            print(f"Si è verificato un errore durante l'installazione dei pacchetti di terze parti: {e}")
