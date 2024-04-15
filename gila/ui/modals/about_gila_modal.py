5f494f6340381f6102d609617e7ec07867b5428a
fran-00 <fran.mf011@gmail.com>
2024-04-15 19:35:25 +0200

    Add AboutGilaModal class to modals module

---
+++ gila/ui/modals/about_gila_modal.py
@@ -0,0 +1,30 @@
+from PySide6.QtGui import Qt
+from PySide6.QtWidgets import QVBoxLayout, QTextEdit
+
+from .parent_modal import Modal
+
+
+class AboutGilaModal(Modal):
+
+    def __init__(self, window):
+        super().__init__(window)
+        self.setWindowTitle("Informazioni su Gila")
+        self.on_modal_layout()
+
+    def on_modal_layout(self):
+        """ Creates modal layout and calls methods that adds widgets """
+        self.modal_layout = QVBoxLayout(self)
+        self.on_modal_text()
+        self.modal_text.setText("Informazioni su Gila")
+        self.on_about_text()
+        self.on_dismiss_button()
+
+    def on_about_text(self):
+        """ Parse an HTML file to show info to user """
+        with open("storage/about.html", 'r') as file:
+            html_content = file.read()
+        self.text_edit = QTextEdit()
+        self.text_edit.setHtml(html_content)
+        self.text_edit.setReadOnly(True)
+        self.text_edit.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        self.modal_layout.addWidget(self.text_edit)
