066f5e89b6815c1e518eaf3fdadcbff09cc08c6c
fran-00 <fran.mf011@gmail.com>
2024-09-08 23:30:37 +0200

    Add submit_prompt to MistralClient

--- gila/ai/mistral.py
+++ gila/ai/mistral.py
@@ -13,3 +13,19 @@ class MistralClient(APIClient):
 
     def submit_api_key(self):
         self.client = Mistral(api_key=self.get_api_key())
+
+    def submit_prompt(self, prompt):
+        self.chat_history.append({"role": "user", "content": prompt})
+        try:
+            response = self.client.chat.complete(
+                model = self.llm,
+                messages = self.chat_history,
+                temperature=self.temperature,
+                max_tokens=self.max_tokens
+            )
+            answer = response.choices[0].message.content
+            response_info = None
+            self.chat_history.append({"role": "assistant", "content": answer})
+            return True, answer, response_info
+        except ValueError as e:
+            return False, e.message, None
