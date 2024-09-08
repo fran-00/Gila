13f12a4685f02eb4a3a75c5de05c28fd4466d2a8
fran-00 <fran.mf011@gmail.com>
2024-09-08 07:58:23 +0200

    Update file formatting

--- gila/ai/manager.py
+++ gila/ai/manager.py
@@ -12,21 +12,22 @@ from .cohere import CohereClient
 
 
 AVAILABLE_MODELS = {
-        "GPT-4": OpenAIClient("gpt-4"),
-        "GPT-4o": OpenAIClient("gpt-4o"),
-        "GPT-4 Turbo": OpenAIClient("gpt-4-turbo"),
-        "GPT-4o mini": OpenAIClient("gpt-4o-mini"),
-        "DALL-E-2": OpenAIClient("dall-e-2"),
-        "DALL-E-3": OpenAIClient("dall-e-3"),
-        "Gemini Pro": GoogleClient("gemini-pro"),
-        "Cohere Chat": CohereClient("cohere-chat"),
+    "GPT-4": OpenAIClient("gpt-4"),
+    "GPT-4o": OpenAIClient("gpt-4o"),
+    "GPT-4 Turbo": OpenAIClient("gpt-4-turbo"),
+    "GPT-4o mini": OpenAIClient("gpt-4o-mini"),
+    "DALL-E-2": OpenAIClient("dall-e-2"),
+    "DALL-E-3": OpenAIClient("dall-e-3"),
+    "Gemini Pro": GoogleClient("gemini-pro"),
+    "Cohere Chat": CohereClient("cohere-chat"),
 }
 
 COMPANIES = {
-        "OPENAI": OpenAIClient("gpt-4o-mini"),
-        "GOOGLE": GoogleClient("gemini-pro"),
-        "COHERE": CohereClient("cohere-chat"),
+    "OPENAI": OpenAIClient("gpt-4o-mini"),
+    "GOOGLE": GoogleClient("gemini-pro"),
+    "COHERE": CohereClient("cohere-chat"),
 }
+
 
 class AIManager(QObject):
     api_key_is_valid_to_controller = Signal(bool)
@@ -41,37 +42,37 @@ class AIManager(QObject):
         self.get_saved_settings()
 
     def get_saved_settings(self):
-        file_path = 'storage/saved_settings.json'
+        file_path = "storage/saved_settings.json"
         if not os.path.exists(file_path):
             default_data = {
                 "llm_name": "GPT-4o mini",
                 "temperature": 1.0,
-                "max_tokens": 4096
+                "max_tokens": 4096,
             }
-            with open(file_path, 'w') as f:
+            with open(file_path, "w") as f:
                 json.dump(default_data, f)
 
-        with open(file_path, 'r') as f:
+        with open(file_path, "r") as f:
             data = json.load(f)
-            llm_name = data.get('llm_name')
+            llm_name = data.get("llm_name")
             self.client = AVAILABLE_MODELS.get(llm_name)
             self.client.llm_name = llm_name
-            self.client.temperature = data.get('temperature')
-            self.client.max_tokens = data.get('max_tokens')
+            self.client.temperature = data.get("temperature")
+            self.client.max_tokens = data.get("max_tokens")
         self.client.generate_chat_id()
 
     def update_saved_settings(self):
-        with open('storage/saved_settings.json', 'r') as f:
+        with open("storage/saved_settings.json", "r") as f:
             data = json.load(f)
             data["llm_name"] = self.client.llm_name
             data["temperature"] = self.client.temperature
             data["max_tokens"] = self.client.max_tokens
 
-        with open('storage/saved_settings.json', 'w') as file:
+        with open("storage/saved_settings.json", "w") as file:
             json.dump(data, file, indent=4)
 
     def on_api_key(self):
-        """ Called by Controller's new_chat_started_slot, asks client to check if
+        """Called by Controller's new_chat_started_slot, asks client to check if
         API key of the said company is present on the storage/api_keys.json file.
         Returns a boolean with response"""
         if self.client.check_if_api_key(self.client.company) is True:
@@ -81,11 +82,11 @@ class AIManager(QObject):
 
     @Slot(str, float, int)
     def set_new_settings_slot(self, new_llm, new_temperature, new_max_tokens):
-        """ Slot
+        """Slot
         Connected to one signal:
             - controller.selected_client_to_manager
         When triggered, takes new llm name and sets new client on call
-        
+
         next_client is a tuple because the first element is the class instance,
         while the second element is the name of the selected client: you can see
         the reason for this in controller.new_chat_started_slot method
@@ -97,14 +98,13 @@ class AIManager(QObject):
 
     @Slot(str)
     def api_key_slot(self, api_key, company_name):
-        """ Slot
+        """Slot
         Connected to one signal:
             - controller.api_key_to_manager
         When triggered, if company_name is None, manager will send new API key
         to the current client, else it will send it on the client based on that
-        company name.
-        API key is then sent to client to check if is valid and emits a boolean
-        signal with response. If key is valid, calls save_api_key
+        company name. API key is then sent to client to check if is valid and
+        emits a boolean signal with response. If key is valid, calls save_api_key
         """
         client = COMPANIES.get(company_name.upper())
         if client is None:
@@ -118,12 +118,12 @@ class AIManager(QObject):
 
     @Slot(str)
     def restore_chat_from_id_slot(self, chat_id):
-        """ Slot
+        """Slot
         Connected to one signal:
             - controller.loading_saved_chat_id_to_manager
         Reads saved chats settings from file and applies them to client
         """
-        with open(f'storage/saved_data/{chat_id}.pk', 'rb') as file:
+        with open(f"storage/saved_data/{chat_id}.pk", "rb") as file:
             saved_data = pickle.load(file)
             chat = saved_data[chat_id]
         # Due to pickle limitation we have to get the client from its name
@@ -137,20 +137,19 @@ class AIManager(QObject):
         self.client.chat_date = chat["chat_date"]
 
     def on_current_settings(self):
-        """ Return current client's settings """
-        settings = (
+        """Return current client's settings"""
+        return (
             self.client.chat_id,
             self.client.chat_custom_name,
             self.client.llm_name,
             self.client.temperature,
             self.client.max_tokens,
-            self.client.chat_date
+            self.client.chat_date,
         )
-        return settings
 
     def save_api_key(self, api_key, company_name):
-        """ Saves validated API Key to api_keys.json file """
-        with open('.env', 'a') as file:
+        """Saves validated API Key to api_keys.json file"""
+        with open(".env", "a") as file:
             file.write(f"\n{company_name.upper()}_API_KEY='{api_key}'")
 
     def check_internet_connection(self):
@@ -161,17 +160,17 @@ class AIManager(QObject):
             return False
 
     def save_current_chat(self):
-        date = datetime.now().strftime(f"%d-%m-%y %H:%M:%S")
+        date = datetime.now().strftime("%d-%m-%y %H:%M:%S")
         data = {
             self.client.chat_id: {
-                "chat_custom_name" : self.client.chat_custom_name,
+                "chat_custom_name": self.client.chat_custom_name,
                 "llm_name": self.client.llm_name,
                 "temperature": self.client.temperature,
                 "max_tokens": self.client.max_tokens,
                 "chat_date": date,
                 "chat_history": self.client.chat_history,
-                "chat_log": None
+                "chat_log": None,
             }
         }
-        with open(f'storage/saved_data/{self.client.chat_id}.pk', 'wb') as file:
+        with open(f"storage/saved_data/{self.client.chat_id}.pk", "wb") as file:
             pickle.dump(data, file)
