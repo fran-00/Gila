17f25bc2c816e64e2f56a1a183990dd86ced968c
fran-00 <fran.mf011@gmail.com>
2025-02-24 15:31:33 +0100

    Add _set_system_message and _build_default_request_data to AnthropicClient

--- gila/ai/anthropic.py
+++ gila/ai/anthropic.py
@@ -11,6 +11,21 @@ class AnthropicClient(APIClient):
     def _get_endpoint(self):
         return "https://api.anthropic.com/v1/messages"
 
+    def _set_system_message(self):
+        """System prompt must be set as a top-level system parameter,
+        so we must override _build_default_request_data method too"""
+        return []
+
+    def _build_default_request_data(self):
+        """Creates request data adding <system> paraneter."""
+        return {
+            "model": self.llm,
+            "messages": self.chat_history,
+            "temperature": self.temperature,
+            "max_tokens": self.max_tokens,
+            "system": self.system_message,
+        }
+
     def _get_request_params(self):
         endpoint = self._get_endpoint()
         headers = {
