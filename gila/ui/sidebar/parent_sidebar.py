998366a2a4b9370119faaad6234e345cfe05ac40
fran-00 <fran.mf011@gmail.com>
2025-02-24 20:21:36 +0100

    Update Sidebar class to show a menu to change settings adding another tab using QTabWidget

--- gila/ui/sidebar/parent_sidebar.py
+++ gila/ui/sidebar/parent_sidebar.py
@@ -1,5 +1,5 @@
-from PySide6.QtCore import QObject, Signal, Slot
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton
+from PySide6.QtCore import QObject, Signal
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QTabWidget
 
 from .stored_chats import StoredChats
 from .change_settings import ChangeSettings
@@ -14,31 +14,35 @@ class Sidebar(QObject):
         self.window = window
         self.widget_container = QWidget(objectName="sidebar_container")
         self.widget_container.setFixedWidth(300)
+
         self.stored_chats = StoredChats(self)
         self.current_settings = CurrentSettings(self.widget_container)
         self.change_settings = ChangeSettings(self, self.current_settings)
+
         self.on_sidebar_container()
 
     def on_sidebar_container(self):
-        """ Creates Sidebar layout and calls methods that adds widgets """
+        """Create sidebar layout using QTabWidget to manage tabs"""
         sidebar_layout = QVBoxLayout(self.widget_container)
-        sidebar_layout.addWidget(self.stored_chats.scroll_area)
+
+        self.tab_widget = QTabWidget()
+        self.tab_widget.addTab(self.stored_chats.scroll_area, "Saved Chats")
+        self.tab_widget.addTab(self.change_settings.scroll_area, "Settings")
+        sidebar_layout.addWidget(self.tab_widget)
+
         sidebar_layout.addWidget(self.current_settings.widget_container)
         sidebar_layout.addWidget(self.on_new_chat_button())
+
         self.current_settings.on_hide_widgets()
-        self.on_hide_widgets()
 
     def on_new_chat_button(self):
-        """ Creates a button to start a new chat """
+        """Creates a button to start a new chat"""
         self.new_chat_button = QPushButton("New Chat")
         self.new_chat_button.clicked.connect(self.send_stop_chat_to_controller)
         return self.new_chat_button
 
-    def open_change_settings_modal(self):
-        self.change_settings_modal.exec_()
-
     def send_stop_chat_to_controller(self):
-        """ Sends a signal to stop current chat, connected to New Chat button"""
+        """Sends a signal to stop current chat, connected to New Chat button"""
         self.stop_chat_to_controller.emit()
 
     def on_show_widgets(self):
