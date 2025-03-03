56f8628b95e4c71cb62c30c6f761485866c4397b
fran-00 <fran.mf011@gmail.com>
2025-03-03 20:50:07 +0100

    Update main function to check if essential files are in storage folder: if they aren't, shows a QMessageBox and close the applicaztion

--- gila/__main__.py
+++ gila/__main__.py
@@ -1,7 +1,8 @@
 import sys
+import os
 import time
 
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QMessageBox
 
 from gila.core.model import Model
 from gila.ui.view import View, LoadingScreen
@@ -11,6 +12,20 @@ from gila.ai.manager import AIManager
 
 def main():
     app = QApplication(sys.argv)
+
+    required_files = [
+        "storage/models.json",
+        "storage/about.html",
+        "storage/assets/css/chatlog-styles.css",
+        "storage/assets/css/spinner.css",
+        "storage/assets/css/styles.css",
+        "storage/assets/js/scroller.js",
+    ]
+
+    if missing_files := [f for f in required_files if not os.path.exists(f)]:
+        error_msg = "The following essential files are missing:\n" + "\n".join(missing_files)
+        QMessageBox.critical(None, "Critical Error", error_msg + "\nGila cannot start.")
+        sys.exit(1)
 
     splash = LoadingScreen()
     splash.show()
