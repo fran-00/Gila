e47f66497854e1cbcb02b791e5a878a71e89c086
fran-00 <fran.mf011@gmail.com>
2025-04-25 10:17:04 +0200

    Add on_show_download_progress_slot, on_show_download_finished_slot and on_show_download_error_slot to DownloadUpdateModal

--- gila/ui/modals/download_update_modal.py
+++ gila/ui/modals/download_update_modal.py
@@ -10,3 +10,27 @@ class DownloadUpdateModal(Modal):
         super().__init__(window)
         self.setWindowTitle("Download Update")
         self.on_modal_layout()
+
+    @Slot(int)
+    def on_show_download_progress_slot(self, percent):
+        """ Slot
+        Connected to one signal:
+            - controller.download_progress_to_view
+        """
+        pass
+
+    @Slot(str)
+    def on_show_download_finished_slot(self, downloaded_path):
+        """ Slot
+        Connected to one signal:
+            - controller.download_finished_to_view
+        """
+        pass
+
+    @Slot(str)
+    def on_show_download_error_slot(self, error_msg):
+        """ Slot
+        Connected to one signal:
+            - controller.download_error_to_view
+        """
+        pass
