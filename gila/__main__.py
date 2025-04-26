37c16cb50d07c4f32ea565dc1e57b81e8314693d
fran-00 <fran.mf011@gmail.com>
2025-04-26 21:28:10 +0200

    Remove required files from main function

--- gila/__main__.py
+++ gila/__main__.py
@@ -13,20 +13,6 @@ from gila.ai.manager import AIManager
 def main():
     app = QApplication(sys.argv)
 
-    required_files = [
-        "storage/models.json",
-        "storage/assets/html/about.html",
-        "storage/assets/css/chatlog-styles.css",
-        "storage/assets/css/spinner.css",
-        "storage/assets/css/styles.css",
-        "storage/assets/js/scroller.js",
-    ]
-
-    if missing_files := [f for f in required_files if not os.path.exists(f)]:
-        error_msg = "The following essential files are missing:\n" + "\n".join(missing_files)
-        QMessageBox.critical(None, "Critical Error", error_msg + "\nGila cannot start.")
-        sys.exit(1)
-
     splash = LoadingScreen()
     splash.show()
     time.sleep(2)
