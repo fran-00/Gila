816c3bac1869c0a01013aa9c0debd89eb446e5b6
fran-00 <fran.mf011@gmail.com>
2025-04-27 11:59:59 +0200

    Remove LoadingScreen class and logic to display it from __main__.py

--- gila/__main__.py
+++ gila/__main__.py
@@ -2,10 +2,10 @@ import sys
 import os
 import time
 
-from PySide6.QtWidgets import QApplication, QMessageBox
+from PySide6.QtWidgets import QApplication
 
 from gila.core.model import Model
-from gila.ui.view import View, LoadingScreen
+from gila.ui.view import View
 from gila.core.controller import Controller
 from gila.ai.manager import AIManager
 
@@ -13,17 +13,12 @@ from gila.ai.manager import AIManager
 def main():
     app = QApplication(sys.argv)
 
-    splash = LoadingScreen()
-    splash.show()
-    time.sleep(2)
-
     manager = AIManager()
     view = View()
     model = Model(manager)
     controller = Controller(model, view)
     view.show()
 
-    splash.finish(view)
     sys.exit(app.exec())
 
 if __name__ == "__main__":
