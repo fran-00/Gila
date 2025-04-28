918fe7942a476552e3b44f65fbc2f88ee871c576
fran-00 <fran.mf011@gmail.com>
2025-04-28 17:30:27 +0200

    Add math and re to imports and update Tokenizer's constructor

--- gila/ui/utils/tokenizer.py
+++ gila/ui/utils/tokenizer.py
@@ -1,9 +1,12 @@
+import math
+import re
 
 
 class Tokenizer:
 
-    def __init__(self):
-        pass
+    def __init__(self, avg_chars_per_token: float = 4.2, word_threshold: int = 6):
+        self.avg_chars_per_token = avg_chars_per_token
+        self.word_threshold = word_threshold
 
     def get_num_of_tokens(self, text):
         pass
