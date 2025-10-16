08cadbd3d2510b978cb0cf5fcac6ed3d4bfd1ceb
fran-00 <fran.mf011@gmail.com>
2025-10-16 11:56:58 +0200

    Refactor LLMSettingsWidget layout and add _build_llms_settings method

--- gila/ui/sidebar/widgets/llm_settings_widget.py
+++ gila/ui/sidebar/widgets/llm_settings_widget.py
@@ -1,5 +1,7 @@
 from PySide6.QtCore import Signal, Qt
 from PySide6.QtWidgets import (
+    QComboBox,
+    QLabel,
     QWidget,
     QVBoxLayout,
 )
@@ -15,5 +17,19 @@ class LLMSettingsWidget(QWidget):
         self._build_layout()
 
     def _build_layout(self):
-        layout = QVBoxLayout(self)
-        layout.setAlignment(Qt.Alignment.AlignTop)
+        self.layout = QVBoxLayout(self)
+        self.layout.setAlignment(Qt.Alignment.AlignTop)
+        self._build_llms_settings()
+        self.parent_cls.add_line_separator(self.layout)
+
+    def _build_llms_settings(self):
+        """ Creates ComboBox with llms list """
+        select_llm_lbl = QLabel("Model")
+        self.parent_cls.parent_cls.window.assign_css_class(select_llm_lbl, "setting_name")
+        select_llm_lbl.setAlignment(Qt.Alignment.AlignCenter)
+        self.llms_combobox = QComboBox()
+        for llm in self.parent_cls.current_settings.llms:
+            self.llms_combobox.addItem(llm)
+        self.llms_combobox.setCurrentIndex(-1)
+        self.layout.addWidget(select_llm_lbl)
+        self.layout.addWidget(self.llms_combobox)
